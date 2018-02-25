3fd838a05d6ef43387cdd417e8997179c8d30b20
dinokraljevic8 <dkralje1@gmail.com>
2018-02-25 20:27:26 +0100

    stavljamo noindex, nofollow

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,8 +69,8 @@ const config = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-      { name='robots' content='noindex, nofollow'>}
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { name:'robots', content:'noindex, nofollow'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
